74774029892c8dd4824b8d85cc859cc0d47ed323
Markus Reiter <me@reitermark.us>
2017-03-02 01:49:04 +0100

    Set `GEM_HOME` and `--bindir` in Bash.

--- etc/rbenv.d/exec/system-ruby.bash
+++ etc/rbenv.d/exec/system-ruby.bash
@@ -1,1 +1,14 @@
-export RUBYLIB="${BASH_SOURCE%.bash}:$RUBYLIB"
+if [ "$RBENV_VERSION" = 'system' ]; then
+  export RUBYLIB="${BASH_SOURCE%.bash}:$RUBYLIB"
+  export GEM_HOME="$RBENV_ROOT/versions/system/lib/ruby/system"
+
+  case "${1}" in
+    gem)
+      case "${2}" in
+        install|uninstall)
+          set -- ${@:1:2} --bindir "$RBENV_ROOT/versions/system/bin" ${@:3}
+        ;;
+      esac
+    ;;
+  esac
+fi
--- etc/rbenv.d/exec/system-ruby/rubygems_plugin.rb
+++ etc/rbenv.d/exec/system-ruby/rubygems_plugin.rb
@@ -1,20 +1,16 @@
-if ENV["RBENV_VERSION"] == 'system' && !(rbenv_root = `rbenv root`.chomp).empty?
-  ENV["GEM_HOME"] = "#{rbenv_root}/versions/system"
-
-  hook = lambda do |installer|
-    begin
-      if installer.spec.executables.any?
-        `rbenv rehash`
-      end
-    rescue
-      warn "rbenv: error in system-ruby (#{$!.class.name}: #{$!.message})"
-    end
-  end
-
+hook = lambda do |installer|
   begin
-    Gem.post_install(&hook)
-    Gem.post_uninstall(&hook)
+    if installer.spec.executables.any?
+      `rbenv rehash`
+    end
   rescue
-    warn "rbenv: error installing system-ruby hooks (#{$!.class.name}: #{$!.message})"
+    warn "rbenv: error in system-ruby (#{$!.class.name}: #{$!.message})"
   end
+end
+
+begin
+  Gem.post_install(&hook)
+  Gem.post_uninstall(&hook)
+rescue
+  warn "rbenv: error installing system-ruby hooks (#{$!.class.name}: #{$!.message})"
 end
